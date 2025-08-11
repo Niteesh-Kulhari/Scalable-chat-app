223193f0c3e57d443c764ebcdde98c17663c2b71
Niteesh kulhari <niteeshkulhari@Niteeshs-Mac-mini.local>
2025-08-10 23:23:43 -0400

    Added relayer

---
+++ relayer-ws/index.ts
@@ -0,0 +1,11 @@
+import { WebSocket, WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({port: 8081})
+
+wss.on('connection', function connection(ws){
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data: string){
+        
+    })
+})
